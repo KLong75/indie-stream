c5b6c75b68d38d0e7213f0695119d7c642277192
Kevin Long <98487770+KLong75@users.noreply.github.com>
2025-03-12 19:09:00 -0500

    add some audio to work with

--- app/ui/audio-player-custom-controls.tsx
+++ app/ui/audio-player-custom-controls.tsx
@@ -4,8 +4,8 @@ import { useState, useRef, useEffect } from "react";
 
 export default function CustomAudioPlayer() {
   const songs = [
-    "/audio/starting-over/mp3/last-night.mp3",
-    "/audio/starting-over/mp3/starting-over.mp3",
+    "/audio/music/the-long-emergency/starting-over/mp3/last-night.mp3",
+    "/audio/music/the-long-emergency/starting-over/mp3/starting-over.mp3",
   ];
   const [currentSongIndex, setCurrentSongIndex] = useState(0);
   const audioRef = useRef<HTMLAudioElement>(null);
