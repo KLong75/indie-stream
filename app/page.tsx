b0c94ba71edad1cd911fe2cf3492736a068fee3b
Kevin Long <98487770+KLong75@users.noreply.github.com>
2025-03-14 17:21:37 -0500

    create image uploader with uploadthing

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,14 @@ import Image from "next/image";
 import AudioPlayer from "./ui/audio-player";
 import CustomAudioPlayer from "./ui/audio-player-custom-controls";
 import LocalAudioPlayer from "./ui/local-audio-player";
-
+import UploadButtonContainer from "./ui/upload-button-container";
 export default function Home() {
   return (
     <div>
-        {/* <AudioPlayer /> */}
-        <CustomAudioPlayer />
-        <LocalAudioPlayer />
+      {/* <AudioPlayer /> */}
+      {/* <LocalAudioPlayer /> */}
+      <CustomAudioPlayer />
+      <UploadButtonContainer />
     </div>
   );
 }
---
+++ app/ui/upload-button-container.tsx
@@ -0,0 +1,22 @@
+"use client";
+//import components
+import { UploadButton } from "./uploadthing/uploadthing";
+
+export default function UploadButtonContainer() {
+  return (
+    <div>
+      <UploadButton
+        endpoint="imageUploader"
+        onClientUploadComplete={(res) => {
+          // Do something with the response
+          console.log("Files: ", res);
+          alert("Upload Completed");
+        }}
+        onUploadError={(error: Error) => {
+          // Do something with the error.
+          alert(`ERROR! ${error.message}`);
+        }}
+      />
+    </div>
+  );
+}
