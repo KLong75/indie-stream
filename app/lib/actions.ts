e6994a758171beca3a9d6c9e8566a921dd0abbf6
Kevin Long <98487770+KLong75@users.noreply.github.com>
2025-03-16 01:25:55 -0500

    comment out uuid import

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,7 +7,7 @@ import { redirect } from 'next/navigation';
 import { signIn } from '@/auth';
 import AuthError from 'next-auth';
 import bcrypt from 'bcrypt';
-import { UUID } from 'crypto';
+// import { UUID } from 'crypto';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
