44abb4cbd0b9dc23bd8cd439db73a1db550ac2ee
Kevin Long <98487770+KLong75@users.noreply.github.com>
2025-03-21 15:24:49 -0500

    artist page

--- app/artists/page.tsx
+++ app/artists/page.tsx
@@ -1,3 +1,17 @@
+// get data
+import {
+  getUserById,
+  getArtistById,
+  getSongById,
+  getReleaseById,
+  getAllSongs,
+  getAllArtists,
+  getAllReleases,
+} from "@/app/lib/data";
+// import definitions
+import { Artist, Playlist, Release, Song } from "@/app/lib/definitions";
+
+
 export default function ArtistsPage() {
   return <div>Artists Page</div>;
 }
