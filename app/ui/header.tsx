f8281d06cdb50f0d0dd527e60e519e035aa3b787
Kevin Long <98487770+KLong75@users.noreply.github.com>
2025-03-22 13:56:17 -0500

    header bg color

--- app/ui/header.tsx
+++ app/ui/header.tsx
@@ -8,7 +8,7 @@ export default async function Header() {
   const session = await auth();
   console.log("Session in Header:", session);
   return (
-    <header className="bg-slate-600 p-4">
+    <header className="bg-gray-900 p-4">
       <h1 className="text-white text-2xl">Indie Stream</h1>
       <Nav />
       {/* <p className="text-white text-sm">
