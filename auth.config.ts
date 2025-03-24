9bbbc4104c0fdf6760951d70de0347a75b7b3e34
Kevin Long <98487770+KLong75@users.noreply.github.com>
2025-03-24 08:13:45 -0500

    update auth

--- auth.config.ts
+++ auth.config.ts
@@ -1,9 +1,9 @@
 import type { NextAuthConfig } from 'next-auth';
 
 export const authConfig = {
-  // pages: {
-  //   signIn: '/login',
-  // },
+  pages: {
+    signIn: '/login',
+  },
   providers: [
     // added later in auth.ts since it requires bcrypt which is only compatible with Node.js
     // while this file is also used in non-Node.js environments
