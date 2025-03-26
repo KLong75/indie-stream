2d6f81cdc20cbb19855f905daace292217eceff1
Kevin Long <98487770+KLong75@users.noreply.github.com>
2025-03-25 20:44:27 -0500

    add ssr to upload thing upload buttons

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
-// import from next-auth
+// import from uploadthing
+import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
+import { extractRouterConfig } from "uploadthing/server";
+import { ourFileRouter } from "../app/api/uploadthing/core";
 
 // import { Geist, Geist_Mono } from "next/font/google";
 //import css
@@ -31,6 +34,15 @@ export default function RootLayout({
       <body
       // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <NextSSRPlugin
+          /**
+           * The `extractRouterConfig` will extract **only** the route configs
+           * from the router to prevent additional information from being
+           * leaked to the client. The data passed to the client is the same
+           * as if you were to fetch `/api/uploadthing` directly.
+           */
+          routerConfig={extractRouterConfig(ourFileRouter)}
+        />
         <Header />
         {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,6 @@ import UploadButtonContainer from "./ui/upload-button-container";
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
       <Link href="/login">Login</Link>
       {/* <CustomAudioPlayer /> */}
       <UploadButtonContainer file_route="audioUploader" />
