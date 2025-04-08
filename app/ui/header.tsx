18df28a3ea2a531166e323ccfef8de903354fcf3
Kevin Long <98487770+KLong75@users.noreply.github.com>
2025-04-07 20:24:48 -0500

    mobile menu

--- app/ui/header.tsx
+++ app/ui/header.tsx
@@ -14,10 +14,11 @@ export default async function Header() {
     <header className="bg-gray-900 p-4 flex justify-between items-center">
       <Link href="/"><h1 className="text-white text-2xl">Indie Stream</h1></Link>
       <div className="flex items-center space-x-4">
+        <div className="hidden md:flex">
         <Nav />
-        {/* <MobileMenu /> */}
-        <MobileMenu session={session} />
+        </div>
         {session ? <SignOutButton /> : null}
+        <MobileMenu session={session} />
       </div>
     </header>
   );
