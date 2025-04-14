aa79cf3cb835a8f3bbd2d92c902a83066612a828
Kevin Long <98487770+KLong75@users.noreply.github.com>
2025-04-13 20:00:03 -0500

    add light and dark theme toggle

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,10 @@ import type { Metadata } from "next";
 import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
 import { extractRouterConfig } from "uploadthing/server";
 import { ourFileRouter } from "../app/api/uploadthing/core";
-
+// import theme provider
+import { ThemeProvider } from "@/components/theme-provider";
+// import components
+import { ModeToggle } from "./ui/mode-toggle";
 // import { Geist, Geist_Mono } from "next/font/google";
 //import css
 import "./globals.css";
@@ -29,15 +32,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
       // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <NextSSRPlugin routerConfig={extractRouterConfig(ourFileRouter)} />
         {/* <Header /> */}
         {/* <main> */}
-
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <ModeToggle />
         {children}
+        </ThemeProvider>
         {/* </main> */}
         {/* <Footer /> */}
       </body>
