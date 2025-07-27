9d4b427f44ec3d1e5f563c7d3e4b393c61622528
Kevin Long <98487770+KLong75@users.noreply.github.com>
2025-07-26 19:09:29 -0500

    add loading pages

--- app/artists/loading.tsx
+++ app/artists/loading.tsx
@@ -2,7 +2,7 @@ export default function Loading() {
   return (
     <div className="flex flex-1 items-center justify-center ">
       <div className="w-80">
-        <p className="text-lg">Loading...</p>
+        <p className="text-lg">Loading artist gallery...</p>
       </div>
     </div>
   );
---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+export default function NotFound() {
+  return(
+    <div>
+      <div className="flex flex-1 items-center justify-center">
+        <div className="w-80">
+          <p className="text-lg">Page not found</p>
+        </div>
+      </div>
+    </div>
+  )
+}
