e565375486fc2b728cc90b61bae1239f423ab7fa
Kevin Long <98487770+KLong75@users.noreply.github.com>
2025-11-07 08:35:40 -0600

    update

---
+++ components/ui/combo-box.tsx
@@ -0,0 +1,93 @@
+"use client";
+
+import * as React from "react";
+// import { Check, ChevronsUpDown } from "lucide-react";
+import { ChevronsUpDown } from "lucide-react";
+import Link from "next/link";
+
+// import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+interface Option {
+  href: string;
+  value: string;
+  option_label: string;
+}
+
+interface ComboboxProps {
+  options: Option[];
+  list_label: string;
+  hasLink?: boolean; // Determines if options should be wrapped in a Link
+  // href?: (value: string) => string; // Function to generate the href dynamically
+  // href?: string; // Static href for the link
+}
+
+export function Combobox({
+  options,
+  list_label,
+  hasLink = false,
+}: // href,
+ComboboxProps) {
+  const [open, setOpen] = React.useState(false);
+  const [value, setValue] = React.useState("");
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+          className="w-[200px] justify-between">
+          {value
+            ? options.find((option) => option.value === value)?.option_label
+            : list_label}
+          <ChevronsUpDown className="ml-auto h-4 w-4 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[200px] p-0">
+        <Command>
+          <CommandInput
+            placeholder={`Search ${list_label}...`}
+            className="h-9"
+          />
+          <CommandList>
+            <CommandEmpty>No {list_label} found.</CommandEmpty>
+            <CommandGroup>
+              {options.map((option) => (
+                <CommandItem
+                  key={option.value}
+                  onSelect={() => {
+                    setValue(option.value);
+                    setOpen(false);
+                  }}
+                >
+                  {hasLink && option.href ? (
+                    <Link href={option.href} className="w-full">
+                      {option.option_label}
+                    </Link>
+                  ) : (
+                    option.option_label
+                  )}
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
+}
