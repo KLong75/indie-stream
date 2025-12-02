732655040560ed6bfb6f6af3285578ebff0faa75
Kevin Long <98487770+KLong75@users.noreply.github.com>
2025-12-02 08:38:52 -0600

    update

--- ui/audio-player-wrapper.tsx
+++ ui/audio-player-wrapper.tsx
@@ -1013,7 +1013,7 @@ export default function AudioPlayerWrapper({
       }
     >
       <div className="w-full max-w-2xl mx-auto">
-        <div className="flex flex-col">
+        <div className="flex flex-col ">
           {/* Always render AudioPlayer so playback continues */}
           <AudioPlayer
             songs={currentSongs}
