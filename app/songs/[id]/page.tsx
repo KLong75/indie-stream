8e111d472258d02874422699e2c0c46ddf92d6d9
Kevin Long <98487770+KLong75@users.noreply.github.com>
2025-12-11 08:55:59 -0600

    update song page and song data

--- app/songs/[id]/page.tsx
+++ app/songs/[id]/page.tsx
@@ -16,26 +16,39 @@ import {
   getAllReleases,
 } from "@/lib/data";
 
-
 export default async function Page(props: { params: Promise<{ id: string }> }) {
   const { id } = await props.params;
   console.log("Song ID:", id); // Debugging line
   const song = await getSongById(id);
-  console.log("Fetched Song:", song); 
+  console.log("Fetched Song:", song);
   if (!song) {
     return <div>Song not found</div>;
-  } 
+  }
   const songArtist = await getArtistById(song.artist);
   const songRelease = await getReleaseById(song.release);
   const releaseType = songRelease?.type;
 
   return (
     <div>
-      <h2 className="p-4 text-center">{song.title}</h2>
-      <h3 className="p-4 text-center">from the {releaseType} {songRelease?.title}</h3>
-      <h3 className="p-4 text-center">by {songArtist?.name}</h3>
-      
-      
+      <div className="p-4 space-y-2">
+        <h2 className=" text-center">{song.title}</h2>
+        <h3 className="text-center">
+          from the {releaseType}{" "}
+          <Link href={`/releases/${songRelease?.id}`} className="underline">
+            {songRelease?.title}
+          </Link>
+        </h3>
+        <h3 className="text-center">
+          by{" "}
+          <Link href={`/artists/${songArtist?.id}`} className="underline">
+            {songArtist?.name}
+          </Link>
+        </h3>
+      </div>
+      <div className="song-stats px-4 space-x-4 flex justify-center">
+        <span>Plays: {song.number_of_plays}</span>
+        <span>Saves: {song.number_of_saves}s</span>
+      </div>
     </div>
   );
 }
