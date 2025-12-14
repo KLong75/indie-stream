1b72eaa2290b50c91f6d52e1fab7760bd4e66f6e
Kevin Long <98487770+KLong75@users.noreply.github.com>
2025-12-14 10:01:56 -0600

    format

--- app/listeners/[id]/page.tsx
+++ app/listeners/[id]/page.tsx
@@ -88,15 +88,14 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
           {userSavedArtists.length === 0 ? (
             <p className="px-4">You have no saved artists.</p>
           ) : (
-              <ArtistList
-                artists={userSavedArtists}
-                placeholder="Search your saved artists..."
-                userId={userId}
-                action={saveArtist}
-                removeAction={removeSavedArtist}
-                savedArtists={userSavedArtists.map((artist) => artist.id)}
-              />
-
+            <ArtistList
+              artists={userSavedArtists}
+              placeholder="Search your saved artists..."
+              userId={userId}
+              action={saveArtist}
+              removeAction={removeSavedArtist}
+              savedArtists={userSavedArtists.map((artist) => artist.id)}
+            />
           )}
         </div>
       </div>
@@ -154,85 +153,84 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
   );
 }
 
-
 // all artists
 
-  // const allArtistsAlphabeticalOrder = allArtists.sort((a, b) =>
-  // a.name.localeCompare(b.name)
-  // );
-  // console.log("allArtists", allArtistsAlphabeticalOrder);
+// const allArtistsAlphabeticalOrder = allArtists.sort((a, b) =>
+// a.name.localeCompare(b.name)
+// );
+// console.log("allArtists", allArtistsAlphabeticalOrder);
 
-  // all songs
-  // const allSongs = await getAllSongs();
-  // console.log("allSongs", allSongs);
-  // const allSongsAlphabeticalOrder = allSongs.sort((a, b) =>
-  //   a.title.localeCompare(b.title)
-  // );
-  // all releases
-  // const allReleases = await getAllReleases();
-  // const allReleasesAlphabeticalOrder = allReleases.sort((a, b) =>
-  //   a.title.localeCompare(b.title)
-  // );
-  // const formattedAllReleases: { [key: string]: Song[] } = {};
-  // await Promise.all(
-  //   allReleasesAlphabeticalOrder
-  //     .filter((release) => release !== null)
-  //     .map(async (release) => {
-  //       // Assign the array directly
-  //       formattedAllReleases[release!.title] = await formatPlaylist({
-  //         playlist: release!,
-  //       });
-  //     })
-  // );
-  // saved artists
-  // const savedArtists = (
-  //   await Promise.all((user.saved_artists || []).map((id) => getArtistById(id)))
-  // ).sort((a, b) => (a?.name || "").localeCompare(b?.name || ""));
-  // console.log("savedArtists", savedArtists);
-  // saved songs
-  // const savedSongs = await Promise.all(
-  //   (user.saved_songs || []).map((id) => getSongById(id))
-  // );
-  // saved releases
-  // const savedReleases = await Promise.all(
-  //   (user.saved_releases || []).map((id) => getReleaseById(id))
-  // );
-  // const formattedSavedReleases: { [key: string]: Song[] } = {};
-  // await Promise.all(
-  //   savedReleases
-  //     .filter((release) => release !== null)
-  //     .map(async (release) => {
-  //       // Assign the array directly
-  //       formattedSavedReleases[release!.title] = await formatPlaylist({
-  //         playlist: release!,
-  //       });
-  //     })
-  // );
-  // user playlists
-  // const playlists = await Promise.all(
-  //   (user.playlists || []).map((id) => getPlaylistById(id))
-  // );
-  // const formattedPlaylists: { [key: string]: Song[] } = {};
-  // await Promise.all(
-  //   playlists
-  //     .filter((pl) => pl !== null)
-  //     .map(async (playlist) => {
-  //       // Assign the array directly
-  //       formattedPlaylists[playlist!.title] = await formatPlaylist({
-  //         playlist: playlist!,
-  //       });
-  //     })
-  // );
-  // public playlists
-  // const publicPlaylists = await getAllPublicPlaylists();
-  // const formattedPublicPlaylists: { [key: string]: Song[] } = {};
-  // await Promise.all(
-  //   publicPlaylists
-  //     .filter((pl) => pl !== null)
-  //     .map(async (playlist) => {
-  //       // Assign the array directly
-  //       formattedPublicPlaylists[playlist!.title] = await formatPlaylist({
-  //         playlist: playlist!,
-  //       });
-  //     })
-  // );
+// all songs
+// const allSongs = await getAllSongs();
+// console.log("allSongs", allSongs);
+// const allSongsAlphabeticalOrder = allSongs.sort((a, b) =>
+//   a.title.localeCompare(b.title)
+// );
+// all releases
+// const allReleases = await getAllReleases();
+// const allReleasesAlphabeticalOrder = allReleases.sort((a, b) =>
+//   a.title.localeCompare(b.title)
+// );
+// const formattedAllReleases: { [key: string]: Song[] } = {};
+// await Promise.all(
+//   allReleasesAlphabeticalOrder
+//     .filter((release) => release !== null)
+//     .map(async (release) => {
+//       // Assign the array directly
+//       formattedAllReleases[release!.title] = await formatPlaylist({
+//         playlist: release!,
+//       });
+//     })
+// );
+// saved artists
+// const savedArtists = (
+//   await Promise.all((user.saved_artists || []).map((id) => getArtistById(id)))
+// ).sort((a, b) => (a?.name || "").localeCompare(b?.name || ""));
+// console.log("savedArtists", savedArtists);
+// saved songs
+// const savedSongs = await Promise.all(
+//   (user.saved_songs || []).map((id) => getSongById(id))
+// );
+// saved releases
+// const savedReleases = await Promise.all(
+//   (user.saved_releases || []).map((id) => getReleaseById(id))
+// );
+// const formattedSavedReleases: { [key: string]: Song[] } = {};
+// await Promise.all(
+//   savedReleases
+//     .filter((release) => release !== null)
+//     .map(async (release) => {
+//       // Assign the array directly
+//       formattedSavedReleases[release!.title] = await formatPlaylist({
+//         playlist: release!,
+//       });
+//     })
+// );
+// user playlists
+// const playlists = await Promise.all(
+//   (user.playlists || []).map((id) => getPlaylistById(id))
+// );
+// const formattedPlaylists: { [key: string]: Song[] } = {};
+// await Promise.all(
+//   playlists
+//     .filter((pl) => pl !== null)
+//     .map(async (playlist) => {
+//       // Assign the array directly
+//       formattedPlaylists[playlist!.title] = await formatPlaylist({
+//         playlist: playlist!,
+//       });
+//     })
+// );
+// public playlists
+// const publicPlaylists = await getAllPublicPlaylists();
+// const formattedPublicPlaylists: { [key: string]: Song[] } = {};
+// await Promise.all(
+//   publicPlaylists
+//     .filter((pl) => pl !== null)
+//     .map(async (playlist) => {
+//       // Assign the array directly
+//       formattedPublicPlaylists[playlist!.title] = await formatPlaylist({
+//         playlist: playlist!,
+//       });
+//     })
+// );
